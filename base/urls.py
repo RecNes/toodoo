69d3535666a9950c74745c465d962e6b46a45302
SencerH <sencerhamarat@gmail.com>
2019-04-07 00:11:27 +0300

    Initial commit"

--- base/urls.py
+++ base/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('app/', views.ToDoApp.as_view()),
+
+    path('', views.start_page, name='index'),  # HTML Index page
+
 ]
