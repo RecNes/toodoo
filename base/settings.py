9a17ee483ac75da4d01b83f2af65316e28e86d5d
SencerH <sencerhamarat@gmail.com>
2019-04-07 01:29:39 +0300

    Initial commit"

--- base/settings.py
+++ base/settings.py
@@ -158,6 +158,6 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny', ],
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
