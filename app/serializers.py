a10599fb8d901e15ef06a9d312728f51b68b9de7
SencerH <sencerhamarat@gmail.com>
2019-04-06 22:30:57 +0300

    Initial commit

--- app/serializers.py
+++ app/serializers.py
@@ -1,20 +1,12 @@
 # coding: utf-8
-from django.contrib.auth.models import User
 from rest_framework import serializers
+
 from .models import ToDo
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['user_name', ]
 
 
 class ToDoSerializer(serializers.ModelSerializer):
 
-    user_name = UserSerializer(many=False)
-
     class Meta:
         model = ToDo
         depth = 1
-        fields = ['user_name', 'note', 'added_at', 'done', 'done_at']
+        fields = ['pk', 'note', 'added_at', 'done', 'done_at']
