3f61618d78ac56dc67d1c04a91f9dd380c801edd
SencerH <sencerhamarat@gmail.com>
2019-04-07 02:11:07 +0300

    done_at field altered and signal removed.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,6 @@
 # code: utf-8
+
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.utils import timezone
 
 
 class ToDo(models.Model):
@@ -12,7 +10,7 @@ class ToDo(models.Model):
     note = models.TextField(verbose_name=u"ToDo")
     added_at = models.DateTimeField(verbose_name=u"Added At", auto_now_add=True)
     done = models.BooleanField(verbose_name=u"Done")
-    done_at = models.DateTimeField(verbose_name=u"Done At", auto_now_add=True)
+    done_at = models.DateTimeField(verbose_name=u"Done At", null=True, blank=True, auto_now=True)
 
     def __unicode__(self):
         return u"Todo note ID={}".format(self.pk)
@@ -21,10 +19,3 @@ class ToDo(models.Model):
         verbose_name = u"Todo"
         verbose_name_plural = u"Todos"
         ordering = ['added_at', ]
-
-
-@receiver(post_save, sender=ToDo)
-def add_timestamp_at_done(sender, instance, **kwargs):
-    # When done add timestamp
-    if instance.done:
-        instance.done_at = timezone.now()
