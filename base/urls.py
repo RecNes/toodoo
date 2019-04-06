d98b50c31025bd5a101233913c07928fe7c3a2bb
SencerH <sencerhamarat@gmail.com>
2019-04-07 01:29:49 +0300

    Initial commit"

--- base/urls.py
+++ base/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from app import views
 
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('app/', views.ToDoApp.as_view()),
+    re_path('app/(?P<pk>\w+)/$', views.ToDoApp.as_view()),
 
     path('', views.start_page, name='index'),  # HTML Index page
 
