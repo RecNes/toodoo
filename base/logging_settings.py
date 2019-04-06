923e1d947e82bbcaa3280d8a9c89f34093d91d2a
SencerH <sencerhamarat@gmail.com>
2019-04-06 21:23:18 +0300

    Initial commit

---
+++ base/logging_settings.py
@@ -0,0 +1,57 @@
+"""
+Django logging settings
+"""
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+log_path = os.path.join(BASE_DIR, 'logs/')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'filters': {
+        # 'special': {
+        #     '()': 'project.logging.SpecialFilter',
+        #     'foo': 'bar',
+        # },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+    },
+    'handlers': {
+        'django': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(log_path, 'django.log'),
+            'formatter': 'verbose'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        # 'mail_admins': {
+        #     'level': 'INFO',
+        #     'class': 'django.utils.log.AdminEmailHandler',
+        #     'formatter': 'verbose'
+        #     # 'filters': ['special']
+        # }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['django', 'console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
